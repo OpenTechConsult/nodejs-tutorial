df130eea74a0ab9b2f0320433ff7a82bbd2558d6
Elom Atsou Agboka <opentech.consult@gmail.com>
2023-12-14 00:48:56 +0000

    Node.js Tutorial for Beginners
    Initialized a Node.js project using npm init -y
    Installed nodemon
    Displayed text to the console output
    Experiment with the process.exit() and process.exitcode to show how to
    gacefully exit from an application or how to make the application crash.

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("This is Node.js Tutorial for Beginners");
+console.log("This is the First Tutorial on Node.js");
+console.log("We will cover node.js with command line");
+// process.exit(1);
+process.exitCode = 0;
+
+
